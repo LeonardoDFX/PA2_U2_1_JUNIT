d09084ae4dd004dda1d0979218352bc3c54cbcba
Hernan Varela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-04-10 09:44:38 -0300

    10-04-23 | 09:43
    Se crea el proyecto con la librería JUnit incluida y la clase de test vacía.

---
+++ src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
@@ -0,0 +1,27 @@
+package frgp.utn.edu.ar.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import frgp.utn.edu.ar.main.MatrizAdyacencia;
+
+class MatrizAdyacenciaTest {
+	
+	/// Punto A: agregarElementoTest
+	
+	/// Punto B: agregarElementoSimetriaTest
+	
+	/// Punto C: eliminarElementoTest
+	
+	/// Punto D: eliminarElementoSimetricoTest
+	
+	/// Punto E: contarRelacionesTest
+	
+	/// Punto F: existenTodosLosElementoTest
+	
+	/// Punto G: agregarElementoFilaNegativaTest
+	
+	/// Punto H: agregarElementoColumnaNegativaTest
+	
+	/// Punto I: agregarElementoFueraRangoTest
+	
+}
